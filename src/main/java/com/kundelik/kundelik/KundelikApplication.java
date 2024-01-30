2adbee76aa8cc9627ca8d7aec4263fcd5f6c7d6a
danialedilev <danial.200316@gmail.com>
2024-01-30 11:45:24 +0600

    init

---
+++ src/main/java/com/kundelik/kundelik/KundelikApplication.java
@@ -0,0 +1,13 @@
+package com.kundelik.kundelik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KundelikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KundelikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kundelik/kundelik/KundelikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kundelik.kundelik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KundelikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
